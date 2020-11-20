e5f572dd821b210cb7a15f2a6ae96d008b487b46
Cesar Nunes <cesar.nunes@cornucopia.com.au>
2020-11-20 23:51:00 +1000

    Creating logs dynamically

--- Project 01 - Boxing Match/app.js
+++ Project 01 - Boxing Match/app.js
@@ -7,7 +7,24 @@ new Vue({
 
     },
     methods: {
+        createLogs(playerHit, tysonHit) {
+            console.log("Creating logs...")
 
+            // Player log
+            const playerLog = document.createElement("div")
+            playerLog.className = "base-log player-log"
+            playerLog.innerText = `You took ${playerHit}% of Tyson's energy.`
+
+            // Tyson log
+            const tysonLog = document.createElement("div")
+            tysonLog.className = "base-log tyson-log"
+            tysonLog.innerText = `You lost ${tysonHit}% of your energy.`
+
+            // Append logs to the Log Panel
+            const logPanel = document.querySelector("div.panel.logs")
+            logPanel.appendChild(playerLog)
+            logPanel.appendChild(tysonLog)
+        }
     },
     watch: {
 
