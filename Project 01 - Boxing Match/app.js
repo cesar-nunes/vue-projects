7f02a4db8f67fc6d21f8db258f3d8855f5b52e9d
Cesar Nunes <cesar.nunes@cornucopia.com.au>
2020-11-27 23:01:07 +1000

    Implemented cross and heal functions

--- Project 01 - Boxing Match/app.js
+++ Project 01 - Boxing Match/app.js
@@ -4,12 +4,28 @@ new Vue({
         player: {
             life: "100%",
             minJab: 5,
-            maxJab: 10
+            maxJab: 10,
+            minCross: 12,
+            maxCross: 15,
+            minHeal: 10,
+            maxHeal: 16,
+            getJabHit(getRandomNumber) {
+                return getRandomNumber(this.minJab, this.maxJab)
+            },
+            getCrossHit(getRandomNumber) {
+                return getRandomNumber(this.minCross, this.maxCross)
+            },
+            getHealValue(getRandomNumber) {
+                return getRandomNumber(this.minHeal, this.maxHeal)
+            },
         },
         tyson: {
             life: "100%",
             minHit: 7,
-            maxHit: 12
+            maxHit: 12,
+            getHit(getRandomNumber) {
+                return getRandomNumber(this.minHit, this.maxHit)
+            },
         }
     },
     computed: {
@@ -24,10 +40,10 @@ new Vue({
             console.log("Processing jab...")
 
             // calculate hits
-            const playerHit = this.getPlayerJabHit()
-            const tysonHit = this.getTysonHit()
+            const playerHit = this.player.getJabHit(this.getRandomNumber)
+            const tysonHit = this.tyson.getHit(this.getRandomNumber)
 
-            console.log(`Player hit: ${playerHit}, Tyson hit: ${tysonHit}`)
+            console.log(`Player jab hit: ${playerHit}, Tyson hit: ${tysonHit}`)
 
             // update life bars and life %
             this.updateFighterLife(this.player, tysonHit)
@@ -36,16 +52,49 @@ new Vue({
             console.log(`Player life: ${this.player.life}, Tyson life: ${this.tyson.life}`)
 
             // create logs
-            this.createLogs(playerHit, tysonHit)
+            this.createLogs(playerHit, tysonHit, null)
 
             console.log("Jab processed successfully!")
         },
         processCross() {
             console.log("Processing cross...")
 
+            // calculate hits
+            const playerHit = this.player.getCrossHit(this.getRandomNumber)
+            const tysonHit = this.tyson.getHit(this.getRandomNumber)
+
+            console.log(`Player cross hit: ${playerHit}, Tyson hit: ${tysonHit}`)
+
+            // update life bars and life %
+            this.updateFighterLife(this.player, tysonHit)
+            this.updateFighterLife(this.tyson, playerHit)
+
+            console.log(`Player life: ${this.player.life}, Tyson life: ${this.tyson.life}`)
+
+            // create logs
+            this.createLogs(playerHit, tysonHit, null)
+
+            console.log("Cross processed successfully!")
+
         },
         processHeal() {
             console.log("Processing heal...")
+
+            // calculate player heal value and tyson hit
+            const healValue = this.player.getHealValue(this.getRandomNumber)
+            const tysonHit = this.tyson.getHit(this.getRandomNumber)
+
+            console.log(`Player heal value: ${healValue}, Tyson hit: ${tysonHit}`)
+
+            // update player life bar and life %
+            this.updateFighterLife(this.player, tysonHit - healValue)
+
+            console.log(`Player life: ${this.player.life}, Tyson life: ${this.tyson.life}`)
+
+            // create logs
+            this.createLogs(null, tysonHit, healValue)
+
+            console.log("Heal processed successfully!")
 
         },
         resetMatch() {
@@ -75,40 +124,58 @@ new Vue({
         getRandomNumber(min, max) {
             return Math.floor(Math.random() * (max - min + 1) + min)
         },
-        getPlayerJabHit() {
-            return this.getRandomNumber(this.player.minJab, this.player.maxJab)
-        },
-        getPlayerCrossHit() {
-
-        },
-        getPlayerHeal() {
-
-        },
-        getTysonHit(){
-            return this.getRandomNumber(this.tyson.minHit, this.tyson.maxHit)
-        },
-        updateFighterLife(fighter, opponentHit) {
+        updateFighterLife(fighter, hitReceived) {
             let fighterLife = Number(fighter.life.substring(0, fighter.life.length - 1))
-            fighterLife -= opponentHit
-            fighter.life = fighterLife <= 0 ? "0%" : `${fighterLife}%`
+            fighterLife -= hitReceived
+
+            if (fighterLife >= 100)
+                fighter.life = "100%"
+            else if (fighterLife <= 0)
+                fighter.life = "0%"
+            else
+                fighter.life = `${fighterLife}%`
         },
-        createLogs(playerHit, tysonHit) {
+        createLogs(playerHit, tysonHit, healValue) {
             console.log("Creating logs...")
 
-            // player log
-            const playerLog = document.createElement("div")
-            playerLog.className = "base-log player-log"
-            playerLog.innerText = `You took ${playerHit.toString()}% of Tyson's energy.`
-
-            // tyson log
-            const tysonLog = document.createElement("div")
-            tysonLog.className = "base-log tyson-log"
-            tysonLog.innerText = `You lost ${tysonHit.toString()}% of your energy.`
-
-            // append logs to the log panel
             const logPanel = document.querySelector("div.panel.logs")
-            logPanel.appendChild(playerLog)
-            logPanel.appendChild(tysonLog)
+
+            if (playerHit) {
+                console.log("Both fighters hitted.")
+
+                // player hit log
+                const playerHitLog = document.createElement("div")
+                playerHitLog.className = "base-log player-hit-log"
+                playerHitLog.innerText = `You took ${playerHit.toString()}% of Tyson's energy.`
+
+                // tyson hit log
+                const tysonHitLog = document.createElement("div")
+                tysonHitLog.className = "base-log tyson-hit-log"
+                tysonHitLog.innerText = `You lost ${tysonHit.toString()}% of your energy.`
+
+                // append logs to the log panel
+                logPanel.appendChild(playerHitLog)
+                logPanel.appendChild(tysonHitLog)
+            }
+            else if (healValue) {
+                console.log("Player healed and Tyson hitted.")
+
+                // player heal log
+                const playerHealLog = document.createElement("div")
+                playerHealLog.className = "base-log player-heal-log"
+                playerHealLog.innerText = `You recovered ${healValue}% of your energy.`
+
+                // tyson hit log
+                const tysonHitLog = document.createElement("div")
+                tysonHitLog.className = "base-log tyson-hit-log"
+                tysonHitLog.innerText = `You lost ${tysonHit.toString()}% of your energy.`
+
+                // append logs to the log panel
+                logPanel.appendChild(playerHealLog)
+                logPanel.appendChild(tysonHitLog)
+            }
+
+            console.log("Logs created successfully!")
         }
     },
     watch: {
